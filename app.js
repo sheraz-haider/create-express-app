391043c0b69827769455f34b7da9f73784a0f635
Sheraz Haider <sheraz5006@gmail.com>
2020-08-02 17:55:29 +0500

    Added Morgan package (an http logger)

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const logger = require('morgan'); 
 const routes = require('./routes/index');
 
 // Don't need body-parser package for express apps 4.16+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
 
 // Static file serving
 app.use(express.static(path.join(__dirname, 'public')));
